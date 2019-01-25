6d491df28d973e60c7df54bf53456b53cb5f2453
danielmarbach <daniel.marbach@openplace.net>
2019-01-25 09:19:46 +0100

    RabbitMQ simple pubsub with NSB

---
+++ Subscriber/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.PubSub.Subscriber";
+        var endpointConfiguration = new EndpointConfiguration("Samples.PubSub.Subscriber");
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.UseConventionalRoutingTopology();
+        transport.ConnectionString("host=localhost");
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
